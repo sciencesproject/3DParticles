e9b50abf483e23d44479d7284e59b0849f68fe0b
Futuray <123812211+Futuray-pgm@users.noreply.github.com>
2025-01-19 10:29:40 +0100

    Adding 3rd dimension and random in generation
    Random is predictable

--- main.cpp
+++ main.cpp
@@ -5,28 +5,36 @@
 #include <filesystem>
 using namespace std;
 #include <cmath>
+#include<cstdlib>
 
 #include "group.h"
 #include "particle.h"
 #include "simulation.h"
 #include "vector3D.h"
 
-int total_particles = 100;
+int total_particles = 512;
 
 int main(int argc, char** argv) {
-	Group g(.8);
-	Simulation sim(.8, 15);
+	Group g(.6);
+	Simulation sim(.7, 15);
 	vector<Particle> particles;
 	particles.reserve(total_particles);
 	for (int i = 0; i<total_particles; i++) {
-		particles.push_back(Particle(&g, i%(int)pow(total_particles, 0.5), floor(i/pow(total_particles, 0.5)), 0));
+		srand(i);
+		particles.push_back(Particle(
+			&g,
+			i%(int)ceil(pow(total_particles, 0.3333333)) + ((float)(rand()%100)/100),
+			(int)floor(i/pow(total_particles, 0.3333333))%(int)ceil(pow(total_particles, 0.3333333)) + ((float)(rand()%100)/100),
+			(int)floor(i/(int)floor(pow((int)ceil(pow(total_particles, 0.3333333)), 2))) + ((float)(rand()%100)/100)
+		));
+		cout << particles[i].pos.as_string() << endl;
 		std::remove(((string)("p" + to_string(i) + ".obj")).c_str());
 		ofstream pf("p" + to_string(i) + ".obj");
 		pf.close();
 	}
 	for (auto &p : particles) sim.particles.push_back(&p);
 	
-	for (int i = 0; i<200; i++) {
+	for (int i = 0; i<20; i++) {
 		std::remove(("all" + to_string(i) + ".obj").c_str());
 		ofstream allf("all" + to_string(i) + ".obj");
 		for (auto &p : particles) allf << "v " << p.pos.x << " " << p.pos.y << " " << p.pos.z << endl;
@@ -38,6 +46,7 @@ int main(int argc, char** argv) {
 			pf << "v " << particles[i].pos.x << " " << particles[i].pos.y << " " << particles[i].pos.z << endl;
 			pf.close();
 		}
+		cout << i << endl;
 	}
 	
 	return 0;
